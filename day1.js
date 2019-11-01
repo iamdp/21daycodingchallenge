26ceeb73e2b9a4bf5dd7712812371952e01cc632
David Pham <email@davidpham.ca>
2019-11-01 12:05:32 -0400

    Create day1.js

---
+++ day1.js
@@ -0,0 +1,8 @@
+  const doorToDoor = (volunteers, neighbourhoods) => {
+    // Code here!
+
+    // Remember to return a value
+
+    // Want to see what's going on inside your function? You can use console.log() to debug and print variables to the console below.
+    return Math.ceil(neighbourhoods.length / volunteers.length);
+  }
