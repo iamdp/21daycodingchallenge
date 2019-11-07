269ac15f439d6ff7d4cdcd8debd10dcf1f02bec5
David Pham <email@davidpham.ca>
2019-11-07 07:07:37 -0500

    Create day 7.js

---
+++ day 7.js
@@ -0,0 +1,21 @@
+
+const termTopics = (interviews) => {
+  let count = [
+                  0, // Smart City
+                  0, // Arts Funding
+                  0  // Transportation
+                ];
+                
+  for (const interview of interviews) {
+    if (interview === 'smart city') {
+      count[0]++;
+    }
+    if (interview === 'arts funding') {
+      count[1]++;
+    }
+    if (interview === 'transportation') {
+      count[2]++;
+    }
+  }
+  return count
+}
